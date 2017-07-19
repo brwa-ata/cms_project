27825c6ca17bfbc8faec930eb8d17ed3277cc842
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-20 01:02:51 +0300

    Getting data from database to table

--- admin/posts.php
+++ admin/posts.php
@@ -34,17 +34,33 @@
                           </thead>
 
                           <tbody>
-                             <tr>
-                                <td>10</td>
-                                <td>Brwa</td>
-                                <td>Programmer</td>
-                                <td>PHP</td>
-                                <td>status</td>
-                                <td>image</td>
-                                <td>tags</td>
-                                <td>comment</td>
-                                <td>date</td>
-                             </tr>
+                              <?php
+                                $sql="SELECT * FROM posts";
+                                $show_query=mysqli_query($connection,$sql);
+                                if(!$show_query)
+                                {
+                                  die("QUERY FAILED ". mysqli_error($connection));
+                                }
+                                else
+                                {
+                                  while ($row=mysqli_fetch_assoc($show_query))
+                                  {
+                                    $post_image=$row["post_image"];
+                                      echo '<tr>
+                                              <td>'.$row["post_id"].'</td>
+                                              <td>'.$row["post_author"].'</td>
+                                              <td>'.$row["post_title"].'</td>
+                                              <td>'.$row["post_catagory_id"].'</td>
+                                              <td>'.$row["post_status"].'</td>';
+                                        echo "<td> <img width=130 src='../images/$post_image'></td>"; // bo pshandany rsmy naw databaseaka
+                                        echo '<td>'.$row["post_tags"].'</td>
+                                              <td>'.$row["post_comment_count"].'</td>
+                                              <td>'.$row["post_date"].'</td>
+                                            </tr>';
+                                  }
+                                }
+                              ?>
+
                           </tbody>
                       </table>
                     </div>
