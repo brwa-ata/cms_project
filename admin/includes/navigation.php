c1a3b62beede6b3547698c8c50676d4eebfe10e5
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-19 17:15:37 +0300

    Replace a Bootstrap element to Dropdown

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -38,9 +38,17 @@
             <li>
                 <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
-            
+
             <li>
-                <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
+                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+                <ul id="demo" class="collapse">
+                    <li>
+                        <a href="#">Dropdown Item</a>
+                    </li>
+                    <li>
+                        <a href="#">Dropdown Item</a>
+                    </li>
+                </ul>
             </li>
             <li>
                 <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
