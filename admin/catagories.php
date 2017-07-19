d88833cf229f7aef6ede4ff7da701b5c5f9def03
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-19 23:46:58 +0300

    Calling insert functions

--- admin/catagories.php
+++ admin/catagories.php
@@ -22,24 +22,8 @@
 
                           <?php
                           // INSERTING CATAGORIES
-                            if(isset($_POST['submit']))
-                            {
-                              $cat_title=$_POST['cat_title'];
-                              if(empty($cat_title))
-                              {
-                                echo "This field should not be empty";
-                              }
-                              else
-                              {
-                                $query="INSERT INTO catagory(title)
-                                        VALUES('$cat_title') ";
-                                $create_catagory_query=mysqli_query($connection,$query);
-                                if(!$create_catagory_query)
-                                {
-                                  die("QUERY FAILED". mysqli_error($connection));
-                                }
-                              }
-                            }
+
+                            insert_catagories();
                            ?>
 
                            <!-- ADD CATAGORIES FORM  -->
