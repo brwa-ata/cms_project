51d4b294f60a15c9dab3ce9ec4938db3ac8069ce
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-20 00:27:12 +0300

    create posts.php, Add post table

---
+++ admin/posts.php
@@ -0,0 +1,60 @@
+<?php include 'includes/header.php'; ?>
+    <div id="wrapper">
+
+        <!-- Navigation -->
+
+        <?php include 'includes/navigation.php'; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                      <h1 class="page-header">
+                          Weolcome Admin
+                          <small>Author</small>
+                      </h1>
+                      <table class="table table-bordered table-hover">
+                          <thead>
+                              <thead>
+                                 <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Catagory</th>
+                                    <th>Statuis</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                 </tr>
+                              </thead>
+                          </thead>
+
+                          <tbody>
+                             <tr>
+                                <td>10</td>
+                                <td>Brwa</td>
+                                <td>Programmer</td>
+                                <td>PHP</td>
+                                <td>status</td>
+                                <td>image</td>
+                                <td>tags</td>
+                                <td>comment</td>
+                                <td>date</td>
+                             </tr>
+                          </tbody>
+                      </table>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include 'includes/fotter.php'; ?>
