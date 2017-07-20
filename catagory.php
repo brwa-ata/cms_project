f8829b69c2e5322b6f06e315da5a252595c46ddd
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-20 22:32:37 +0300

    Create a catagory.php file

---
+++ catagory.php
@@ -0,0 +1,66 @@
+<?php  include 'includes/header.php';  ?>
+
+<body>
+
+    <!-- Navigation -->
+    <?php  include 'includes/navigation.php';   ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+
+            <div class="col-md-8">
+
+              <?php
+                  $sql="SELECT * FROM posts";
+                  $ex=mysqli_query($connection,$sql);
+                  while ($row=mysqli_fetch_assoc($ex))
+                   {
+                      $post_id=$row['post_id'];
+                      $post_title=$row['post_title'];
+                      $post_author=$row['post_author'];
+                      $post_date=$row['post_date'];
+                      $post_image=$row['post_image'];
+                      $post_content=$row['post_content'];
+                ?>
+                <!-- First Blog Post -->
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+                <h2>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+                <?php } ?>
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+
+              <?php  include 'includes/sidebar.php';   ?>
+
+        </div>
+      </div>
+        <!-- /.row -->
+
+        <hr>
+        <?php
+          include 'includes/footer.php';
+         ?>
+</body>
+
+</html>
