379f581751ca6aca97a1dfbfa2ecee6416a485df
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-20 22:43:11 +0300

    Fix sum link address

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -7,7 +7,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">CMS Admin</a>
+        <a class="navbar-brand" href="index.php">CMS Admin</a>
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
@@ -36,7 +36,7 @@
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li>
-                <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
 
             <li>
@@ -44,16 +44,16 @@
                   <i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="#">View All Posts</a>
+                        <a href="posts.php">View All Posts</a>
                     </li>
                     <li>
-                        <a href="#">Add Posts</a>
+                        <a href="posts.php?source=add_post">Add Posts</a>
                     </li>
                 </ul>
             </li>
 
             <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Catagories Page</a>
+                <a href="catagories.php"><i class="fa fa-fw fa-wrench"></i> Catagories Page</a>
             </li>
             <li>
                 <a href="javascript:;" data-toggle="collapse" data-target="#demo">
@@ -78,7 +78,7 @@
                         <a href="#">View All Posts</a>
                     </li>
                     <li>
-                        <a href="#">Add Posts</a>
+                        <a href="">Add Posts</a>
                     </li>
                 </ul>
             </li>
