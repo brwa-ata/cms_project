f37133487d03f4443210a375d3d56f2cb94e4153
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-20 12:11:37 +0300

    Include edit_post.php in switch&case

--- admin/posts.php
+++ admin/posts.php
@@ -16,53 +16,37 @@
                           Weolcome Admin
                           <small>Author</small>
                       </h1>
-                      <table class="table table-bordered table-hover">
-                          <thead>
-                              <thead>
-                                 <tr>
-                                    <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Catagory</th>
-                                    <th>Statuis</th>
-                                    <th>Image</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Date</th>
-                                 </tr>
-                              </thead>
-                          </thead>
 
-                          <tbody>
-                              <?php
-                                $sql="SELECT * FROM posts";
-                                $show_query=mysqli_query($connection,$sql);
-                                if(!$show_query)
-                                {
-                                  die("QUERY FAILED ". mysqli_error($connection));
-                                }
-                                else
-                                {
-                                  while ($row=mysqli_fetch_assoc($show_query))
-                                  {
-                                    $post_image=$row["post_image"];
-                                      echo '<tr>
-                                              <td>'.$row["post_id"].'</td>
-                                              <td>'.$row["post_author"].'</td>
-                                              <td>'.$row["post_title"].'</td>
-                                              <td>'.$row["post_catagory_id"].'</td>
-                                              <td>'.$row["post_status"].'</td>';
-                                        echo "<td> <img width=130 src='../images/$post_image'></td>"; // bo pshandany rsmy naw databaseaka
-                                        echo '<td>'.$row["post_tags"].'</td>
-                                              <td>'.$row["post_comment_count"].'</td>
-                                              <td>'.$row["post_date"].'</td>
-                                            </tr>';
-                                  }
-                                }
-                              ?>
+          <?php
+            if(isset($_GET['source']))
+            {
+              $source=$_GET['source'];
+            }
+            else
+            {
+              $source=''; // bo awaya la waz3aka beta darawa
+            }
+            switch ($source)
+            {
+              case 'add_post':
+                include 'includes/add_post.php';
+              break;
 
-                          </tbody>
-                      </table>
+              case 'edit_posts':
+                include 'includes/edit_posts.php';
+              break;
+
+              case '200':
+                echo "nice";
+              break;
+
+              default:
+                include 'includes/view_all_post.php';
+              break;
+            }
+
+          ?>
+
                     </div>
                 </div>
                 <!-- /.row -->
