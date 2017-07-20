1d4701a3fc6192551728ca2fa66da136db4057b5
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-20 21:58:38 +0300

    Add comment section

--- post.php
+++ post.php
@@ -1,33 +1,65 @@
-<?php include 'includes/header.php'; ?>
-    <div id="wrapper">
+<?php  include 'includes/header.php';  ?>
 
-        <!-- Navigation -->
+<body>
 
+    <!-- Navigation -->
+    <?php  include 'includes/navigation.php';   ?>
 
-        <?php include 'includes/navigation.php'; ?>
+    <!-- Page Content -->
+    <div class="container">
 
+        <div class="row">
 
+            <!-- Blog Entries Column -->
 
-        <div id="page-wrapper">
+            <div class="col-md-8">
 
-            <div class="container-fluid">
+              <?php
+                  $sql="SELECT * FROM posts";
+                  $ex=mysqli_query($connection,$sql);
+                  while ($row=mysqli_fetch_assoc($ex))
+                   {
+                      $post_title=$row['post_title'];
+                      $post_author=$row['post_author'];
+                      $post_date=$row['post_date'];
+                      $post_image=$row['post_image'];
+                      $post_content=$row['post_content'];
+                ?>
+                <!-- First Blog Post -->
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Weolcome Admin
-                            <small>Author</small>
-                        </h1>
-
-                    </div>
-                </div>
-                <!-- /.row -->
+                <hr>
+                <?php } ?>
 
             </div>
-            <!-- /.container-fluid -->
+
+            <!-- Blog Sidebar Widgets Column -->
+
+              <?php  include 'includes/sidebar.php';   ?>
 
         </div>
-        <!-- /#page-wrapper -->
+      </div>
+        <!-- /.row -->
 
-<?php include 'includes/fotter.php'; ?>
+        <hr>
+        <?php
+          include 'includes/footer.php';
+         ?>
+</body>
+
+</html>
