a8803748bb5fa8887bbb547fd719c18abe05c345
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-21 02:14:32 +0300

    make a comment

--- post.php
+++ post.php
@@ -15,7 +15,14 @@
             <div class="col-md-8">
 
               <?php
-                  $sql="SELECT * FROM posts";
+                if(isset($_GET['p_id']))
+                {
+                  $post_id=$_GET['p_id'];
+                }
+              ?>
+
+              <?php
+                  $sql="SELECT * FROM posts WHERE post_id= $post_id ";
                   $ex=mysqli_query($connection,$sql);
                   while ($row=mysqli_fetch_assoc($ex))
                    {
@@ -45,6 +52,93 @@
 
                 <hr>
                 <?php } ?>
+
+                <!-- Blog Comments -->
+
+                <?php
+                    if (isset($_POST['create_comment']))
+                    {
+                        $post_id=$_GET['p_id'];
+                        $comment_author=$_POST['comment_author'];
+                        $comment_email=$_POST['comment_email'];
+                        $comment_content=$_POST['comment_content'];
+
+                        $comment_sql="INSERT INTO comments(comment_post_id,comment_author,comment_email,comment_content,comment_status,comment_date) 
+                                      VALUES ($post_id,'$comment_author','$comment_email','$comment_content','unproved',now())";
+                        $ex_comment_sql=mysqli_query($connection,$comment_sql);
+                        if (!$ex_comment_sql){
+                            die("QUERY FAILED". mysqli_error($connection));
+                        }
+
+                    }
+                ?>
+
+                <!-- Comments Form -->
+                <div class="well">
+                    <h4>Leave a Comment:</h4>
+                    <form action="" method="post" role="form">
+
+                        <div class="form-group">
+                            <label for="comment_author">Author</label>
+                            <input class="form-control" type="text" name="comment_author">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="comment_email">Email</label>
+                            <input class="form-control" type="email" name="comment_email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="comment">Your Comment</label>
+                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                        </div>
+                        <button name="create_comment" type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+
+                <hr>
+
+                <!-- Posted Comments -->
+                <!-- Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div>
+
+                <!-- Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <!-- Nested Comment -->
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading">Nested Start Bootstrap
+                                    <small>August 25, 2014 at 9:30 PM</small>
+                                </h4>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            </div>
+                        </div>
+                        <!-- End Nested Comment -->
+                    </div>
+                </div>
+
+
 
             </div>
 
