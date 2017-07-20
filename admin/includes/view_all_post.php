0e8857c18164123e5fb6cd36082314d82e74da45
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-20 18:39:04 +0300

    bringin the catagory title from catagory table
    in order to dsplay the catagory title in the table instead of the number
    of catagory

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -28,25 +28,52 @@
           {
             while ($row=mysqli_fetch_assoc($show_query))
             {
-              $cat_id=$row["post_id"];
+              $post_id=$row["post_id"];
+              $post_author=$row["post_author"];
+              $post_title=$row["post_title"];
+              $post_catagory_id=$row["post_catagory_id"];
+              $post_status=$row["post_status"];
               $post_image=$row["post_image"];
-                echo '<tr>
-                        <td>'.$row["post_id"].'</td>
-                        <td>'.$row["post_author"].'</td>
-                        <td>'.$row["post_title"].'</td>
-                        <td>'.$row["post_catagory_id"].'</td>
-                        <td>'.$row["post_status"].'</td>';
-                  echo "<td> <img width=130 src='../images/$post_image'></td>"; // bo pshandany rsmy naw databaseaka
-                  echo '<td>'.$row["post_tags"].'</td>
-                        <td>'.$row["post_comment_count"].'</td>
-                        <td>'.$row["post_date"].'</td>';
-                  echo   "<td><a href='posts.php?delete={$cat_id}'>Delete</a></td>
-                      </tr>";
+              $post_tags=$row["post_tags"];
+              $post_comment_count=$row["post_comment_count"];
+              $post_date=$row["post_date"];
+
+                 echo '<tr>
+                        <td>'.$post_id.'</td>
+                        <td>'.$post_author.'</td>
+                        <td>'.$post_title.'</td>';
+
+                        // bo henany nawy catagoryiaka la tabley catagory
+                        $sql="SELECT * FROM catagory WHERE id=$post_catagory_id";
+                        $select_catagory=mysqli_query($connection,$sql);
+                        if(!$select_catagory)
+                        {
+                          die("QUERY FAILED" .mysqli_error($connection));
+                        }
+                        else
+                        {
+                            while ($row=mysqli_fetch_assoc($select_catagory))
+                            {
+                              $cat_id=$row['id'];
+                              $cat_title=$row['title'];
+                            }
+                          }
+                  echo    '<td>'.$cat_title.'</td>';
+                  echo    '<td>'.$post_status.'</td>';
+                  echo "<td> <img width=130 src='../images/$post_image'></td>"; // bo pshandany rasmy naw databaseaka
+                  echo '<td>'.$post_tags.'</td>
+                        <td>'.$post_comment_count.'</td>
+                        <td>'.$post_date.'</td>';
+                  echo   "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";// bo away ba $_GET btwanyn (id)y har postek war bgrin
+                  echo   "<td><a href='posts.php?source=edit_posts&p_id={$post_id}'>Edit</a></td>";
+                  echo    "</tr>";//(source) chwnka la posts.php bakarman henawa =edit_posts chwnka la case waman danawa gar source yaksan bw bama
+                                  // ba bcheta pagey edit_posts (&) chwnka $_GET esta 2 key haya (p_id) bo away (id)y postaka warbgrun
+                                  // kawata esta bam shewaya ka (edit)man krd achyna pagey editawa ka atwanin edit bkayn bo aw posta
             }
           }
         ?>
 
-        <?php
+        <?php // DELETE POSTS
             if(isset($_GET['delete'])) // am delete hy (key)akaya ==> ?delete
             {
               $delete_cat_id=$_GET['delete'];
