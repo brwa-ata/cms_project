195648a0a695a236b6bf0657e1eb0af7d5dddf6e
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-22 01:00:56 +0300

    Add some statistic
    add statistic about number of comments,users,posts,categories

--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,134 @@
 
                     </div>
                 </div>
+
                 <!-- /.row -->
+
+
+                <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-file-text fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                        <?php
+                                            $sql="SELECT * FROM posts";
+                                            $select_all_post=mysqli_query($connection,$sql);
+                                            $post_count=mysqli_num_rows($select_all_post); // pet ale ka chan post haya chwnka aw har royak 7sab aka
+                                            echo " <div class='huge'>$post_count</div>";
+
+                                        ?>
+                                        <div>Posts</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="posts.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                        <?php
+                                            $query="SELECT * FROM comments";
+                                            $select_all_comment=mysqli_query($connection,$query);
+                                            $comment_count=mysqli_num_rows($select_all_comment);
+                                            echo "<div class='huge'>$comment_count</div>";
+                                        ?>
+
+                                        <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="comments.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                        <?php
+                                            $query="SELECT * FROM users";
+                                            $select_all_user=mysqli_query($connection,$query);
+                                            $user_count=mysqli_num_rows($select_all_user);
+                                            echo "<div class='huge'>$user_count</div>";
+                                        ?>
+
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                        <?php
+                                            $query="SELECT * FROM catagory";
+                                            $select_all_catagory=mysqli_query($connection,$query);
+                                            $catagory_count=mysqli_num_rows($select_all_catagory);
+                                            echo "<div class='huge'>$catagory_count</div>";
+                                        ?>
+
+                                        <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="catagories.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+
+
 
             </div>
             <!-- /.container-fluid -->
