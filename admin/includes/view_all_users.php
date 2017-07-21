5868c26b39a7437f64dfae22df5f0bd95ed4aa96
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-21 17:56:54 +0300

    Add edit link to update the information about user

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -84,7 +84,7 @@
 
             echo   "<td><a href='users.php?change_to_admin=$user_id'>Admin</a></td>";
             echo   "<td><a href='users.php?change_to_sub=$user_id'>Subscriber</a></td>";
-
+            echo   "<td><a href='users.php?source=edit_user&u_id=$user_id'>Edit</a></td>";
             echo   "<td><a href='users.php?delete=$user_id'>Delete</a></td>";// bo away ba $_GET btwanyn (id)y har postek war bgrin
             echo    "</tr>";
         }
