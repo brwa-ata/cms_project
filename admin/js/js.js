4a7bfd7ab8cc5704f46d7c071d1a429cd9f6d47a
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-22 16:26:17 +0300

    Some Code to check all checkboxes true

--- admin/js/js.js
+++ admin/js/js.js
@@ -2,4 +2,29 @@
  * Created by BRWA on 7/22/2017.
  */
 
-tinymce.init({ selector:'textarea' });
+//tinymce.init({ selector:'textarea' });
+
+// am codea wa aka gar clickman la check boxakay sarawa krd hamw awany trish select bka
+
+$(document).ready(function ()
+{
+
+    $('#selectAllBoxes').click(function (event)
+    {
+        if (this.checked)
+        {
+            $('.checkbox').each(function ()
+            {
+                this.checked = true;
+            });
+        }
+        else
+        {
+            $('.checkbox').each(function ()
+            {
+                this.checked = false;
+            });
+        }
+    });
+
+});
