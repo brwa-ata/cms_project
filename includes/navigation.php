520bc0fd6e3a522650c8b6c8b36aa8dce8d1dd6b
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-22 14:55:15 +0300

    Add (edit post) link to the nav bar to edit the specific post

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,4 +1,5 @@
 <?php include 'db.php'; ?>
+<?php session_start(); ?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -9,7 +10,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Start Bootstrap</a>
+            <a class="navbar-brand" href="index.php">CMS Front</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -22,17 +23,24 @@
                         echo "<li><a href='#'>{$row['title']}</a></li>";
                      }
                ?>
-             <li>
+                <li>
                     <a href="admin">Admin</a>
                 </li>
-<!--   
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
--->
+
+                <?php
+                    if (isset($_SESSION['username']))
+                    {
+                        if (isset($_GET['p_id']))
+                        {
+                            $the_post_id=$_GET['p_id'];
+
+                            echo "<li><a href='admin/posts.php?source=edit_posts&p_id=$the_post_id'>Edit Post</a> </li>";
+                        }
+
+                    }
+
+                ?>
+
             </ul>
         </div>
         <!-- /.navbar-collapse -->
