024ba990e41327823056725dac4bbc55514f36db
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-22 16:27:27 +0300

    Some script code to do javascript

---
+++ admin/includes/fotter.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<script src="js/jquery.js"></script>
+
+<!-- labar check boxakan amaman nwsywa -->
+<script src="js/js.js"></script>
+
+<script src="js/bootstrap.min.js">  </script>
+
+
+</body>
+</html>
