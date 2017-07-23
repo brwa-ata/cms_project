a066733a5e58e9dadbee30064433eac2f21c92d2
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-23 22:42:06 +0300

    Show the username at the top

--- admin/comments.php
+++ admin/comments.php
@@ -14,7 +14,7 @@
                 <div class="col-lg-12">
                     <h1 class="page-header">
                         Weolcome Admin
-                        <small>Author</small>
+                        <small><?php echo $_SESSION['username']; ?></small>
                     </h1>
 
                     <?php
