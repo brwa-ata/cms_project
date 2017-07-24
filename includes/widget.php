aa052e245f987f1995a717407e6ad2140643056c
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-24 14:57:02 +0300

    Change the widget text

--- includes/widget.php
+++ includes/widget.php
@@ -1,6 +1,8 @@
 <div class="well">
     <h4>side widget well</h4>
     <p>
-        cncdsjkcncdlcndcjdsc
+        Hello, My name is Brwa,now iam just a student and still learning things,
+        but INSHALLAH in the next 2 years I will be the best a master of web development
+        because (ALLAH) with me and I know that the KNOWLEDGE is a POWER
     </p>
 </div>
