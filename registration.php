f58e730f4eb599fc294ec70ca29c33f9c65857e3
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-24 19:12:36 +0300

    Add the auto complete to username and password

--- registration.php
+++ registration.php
@@ -56,14 +56,21 @@
             <div class="col-xs-6 col-xs-offset-3">
                 <div class="form-wrap">
                 <h1>Register</h1>
-                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="on">
                         <div class="form-group">
                             <label for="username" class="sr-only">username</label>
-                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                            <input type="text" name="username" id="username" class="form-control"
+                                   placeholder="Enter Desired Username"
+
+                                value="<?php echo isset($username) ? $username : ''; ?>"
+
+                            >
+
                         </div>
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                            <input type="email" name="email" id="email" class="form-control"
+                                   placeholder="somebody@example.com" autocomplete="on">
                         </div>
                          <div class="form-group">
                             <label for="password" class="sr-only">Password</label>
