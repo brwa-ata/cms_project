64b4bc1326e1b8b189a2095d3a659331c9b16606
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-25 15:12:32 +0300

    Sending email to get password
    by using composer and SMTP protocolwe send an email to (mailtrap.io) website which is for development test and perpuse

---
+++ forgot.php
@@ -0,0 +1,133 @@
+<?php  include "includes/db.php"; ?>
+<?php  include "includes/header.php"; ?>
+
+
+<?php
+    // wata gar methodaka $GET nabw wa (key) $_GET (forgot) nabw awa bgarerawa index
+    // ba kwrty wata gar click la forgot password nakrabw wa wysty la rey urlawa daxl be
+    // ba nacheta nawy w bgaretawa bo paray saraky
+   /* if ($_SERVER['REQUEST_METHOD'] != strtoupper('get') || !$_GET['forgot'] )
+    {
+        header("Location: index.php");
+    }
+
+*/
+
+   require './vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+   require './Class/classes/config.php';
+
+    if ($_SERVER['REQUEST_METHOD'] == strtoupper('post'))
+    {
+        if (isset($_POST['email']))
+        {
+            $email=$_POST['email'];
+
+            $length=50;
+
+            // am functiona ba shewayaky random text drwst aka
+            $token=bin2hex(openssl_random_pseudo_bytes($length));
+
+            if (email_exists($email))
+            {
+               if( $stmt=mysqli_prepare($connection, "UPDATE users SET token='$token'  WHERE  user_email=?"))
+               {
+                   mysqli_stmt_bind_param($stmt,"s",$email);
+                   mysqli_stmt_execute($stmt);
+
+                   mysqli_stmt_close($stmt);
+
+                   /***
+                    *
+                    * configure PHPMailer
+                    *
+                    */
+
+                   $mail= new PHPMailer();
+
+                   $mail->isSMTP();                                      // Set mailer to use SMTP
+                   $mail->Host = config::SMTP_HOST;  // Specify main and backup SMTP servers
+                   $mail->Username = config::SMTP_USER;                 // SMTP username
+                   $mail->Password = config::SMTP_PASSWORD;             // SMTP password
+                   $mail->Port = config::SMTP_PORT;                         // TCP port to connect to
+                   $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                   $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                   $mail->isHTML(true);
+
+                   $mail->setFrom('brwa.ata.hamalaw@gmail.com', 'Brwa Ata');
+                   $mail->addAddress($email);
+
+                   $mail->Subject = 'This is a test email';
+
+                   $mail->Body='<h1>Email Body</h1> ';
+
+                   if ($mail->send())
+                   {
+                       echo "Your email was sent";
+                   }
+                   else
+                   {
+                       echo "Your email was NOT sent";
+                   }
+
+
+               }
+
+
+            }
+
+        }
+
+    }
+?>
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="form-gap"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="text-center">
+
+
+                            <h3><i class="fa fa-lock fa-4x"></i></h3>
+                            <h2 class="text-center">Forgot Password?</h2>
+                            <p>You can reset your password here.</p>
+                            <div class="panel-body">
+
+
+
+
+                                <form id="register-form" role="form" autocomplete="off" class="form" method="post">
+
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                            <input id="email" name="email" placeholder="email address" class="form-control"  type="email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
+                                    </div>
+
+                                    <input type="hidden" class="hide" name="token" id="token" value="">
+                                </form>
+
+                            </div><!-- Body-->
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <hr>
+
+    <?php include "includes/footer.php";?>
+
+</div> <!-- /.container -->
+
