ef53d19607588a4c5facc37371ae0d6951fbb924
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-25 17:28:02 +0300

    Reset a password

---
+++ reset.php
@@ -0,0 +1,123 @@
+<?php  include "includes/db.php"; ?>
+<?php  include "includes/header.php"; ?>
+
+
+<?php
+
+    // ama bo awaya ka kas la regay pathawa natwane bcheta naw aw formawa wata illa abe checky
+    //emailakay bka larey aw linkay ka lawe danrawa bcheta naw aw formawa
+    if (!isset($_GET['email']) && !isset($_GET['token']))
+    {
+        header("Location: index.php");
+    }
+
+   // $email="brwa@gmail.com";
+
+    //$token="5a50fa18331a113ee8e2a7790b572c606d23171abf3d72361a1603647981607af701b40f1c88c350c8f037f35dc23da940ed";
+
+    if ($stmt=mysqli_prepare($connection,"SELECT username, user_email,token FROM users WHERE token=?"))
+    {
+        mysqli_stmt_bind_param($stmt,"s",$_GET['token']);
+
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_bind_result($stmt,$username,$user_email,$token);
+
+        mysqli_stmt_fetch($stmt);
+
+        mysqli_stmt_close($stmt);
+
+ /*       if ($_GET['token'] !== $token || $_GET['email'] !== $user_email)
+        {
+            header("Location: index.php")
+        }
+*/
+
+        if (isset($_POST['password']) && isset($_POST['confirmPassword']))
+        {
+            $password=$_POST['password'];
+            $confirmPassword=$_POST['confirmPassword'];
+
+            if ($password == $confirmPassword)
+            {
+
+                $hashed_password=password_hash($password,PASSWORD_BCRYPT,array('cost'=>10));
+                if ($stmt=mysqli_prepare($connection,"UPDATE users SET token='', user_password='$hashed_password' WHERE user_email=? "))
+                {
+                    mysqli_stmt_bind_param($stmt,"s",$_GET['email']);
+
+                    mysqli_stmt_execute($stmt);
+
+                    if (mysqli_stmt_affected_rows($stmt) >=1)
+                    {
+                       header("Location: index.php");
+                    }
+                    mysqli_stmt_close($stmt);
+
+                }
+
+            }
+
+        }
+    }
+?>
+
+
+<!-- Page Content -->
+<div class="container">
+
+
+    <div class="form-gap"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="text-center">
+
+                            <h3><i class="fa fa-lock fa-4x"></i></h3>
+                            <h2 class="text-center">Reset Password</h2>
+                            <p>You can reset your password here.</p>
+                            <div class="panel-body">
+
+                                <form id="register-form" role="form" autocomplete="off" class="form" method="post">
+
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                            <input id="password" name="password" placeholder="Enter password" class="form-control"  type="password">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                            <input id="confirmPassword" name="confirmPassword" placeholder="Confirm password" class="form-control"  type="password">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <input name="resetPassword" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
+                                    </div>
+
+                                    <input type="hidden" class="hide" name="token" id="token" value="">
+                                </form>
+
+                            </div><!-- Body-->
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <hr>
+
+    <?php include "includes/footer.php";?>
+
+</div> <!-- /.container -->
+
